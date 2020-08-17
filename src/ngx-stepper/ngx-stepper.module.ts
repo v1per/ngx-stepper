5b72ed47c2dfa600e66e9b30a73ef15b5ef44e35
Bartosz Chojnacki <bartosz.chojnacki@apptimia.com>
2020-08-17 14:25:07 +0200

    update material imports

--- src/ngx-stepper/ngx-stepper.module.ts
+++ src/ngx-stepper/ngx-stepper.module.ts
@@ -1,43 +1,22 @@
-import {
-  NgModule,
-  NO_ERRORS_SCHEMA
-} from '@angular/core';
-
-import {
-  CommonModule
-} from '@angular/common';
-
-import {
-  MatInputModule,
-  MatButtonModule,
-  MatIconModule,
-  MatDialogModule,
-  MatRippleModule,
-  MatToolbarModule
-} from '@angular/material';
-
-import {
-  NgxStepperComponent
-} from './ngx-stepper.component';
-
-import {
-  NgxStepComponent
-} from './ngx-step.component';
-
-import {
-  NgxStepBodyComponent
-} from './ngx-step-body.component';
-
-import {
-  NgxStepActionsComponent
-} from './ngx-step-actions.component';
+import { CommonModule } from "@angular/common";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatRippleModule } from "@angular/material/core";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { NgxStepActionsComponent } from "./ngx-step-actions.component";
+import { NgxStepBodyComponent } from "./ngx-step-body.component";
+import { NgxStepComponent } from "./ngx-step.component";
+import { NgxStepperComponent } from "./ngx-stepper.component";
 
 @NgModule({
   declarations: [
     NgxStepperComponent,
     NgxStepComponent,
     NgxStepBodyComponent,
-    NgxStepActionsComponent
+    NgxStepActionsComponent,
   ],
   imports: [
     CommonModule,
@@ -46,15 +25,14 @@ import {
     MatButtonModule,
     MatIconModule,
     MatDialogModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
   exports: [
     NgxStepperComponent,
     NgxStepComponent,
     NgxStepBodyComponent,
-    NgxStepActionsComponent
+    NgxStepActionsComponent,
   ],
-  schemas: [ NO_ERRORS_SCHEMA ]
+  schemas: [NO_ERRORS_SCHEMA],
 })
-export class NgxStepperModule {
-}
+export class NgxStepperModule {}
